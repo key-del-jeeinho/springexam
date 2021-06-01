1e71071393d96e0f7409cb7b1bd953f0e44c8a5a
Xylope <XylopeOfficial@gmail.com>
2021-06-02 03:48:23 +0900

    initialize Spring boot project

---
+++ src/test/java/com/xylope/springexam/SpringExamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xylope.springexam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringExamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
